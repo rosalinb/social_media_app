f4f5bb925901b293c71d943bfb9d326b26a905c7
rosalinb <rosalinbiswal009@gmail.com>
2022-06-07 14:16:03 +1000

    changes in signin and login

--- client/js/components/sign_up.js
+++ client/js/components/sign_up.js
@@ -1,46 +1,42 @@
 function renderSignUp() {
-  document.querySelector('#page').innerHTML = `
-    <section class="sign-up">
+  document.querySelector("#page").innerHTML = `
+    <section class="sign-up-form">
       <form onSubmit="singUp(event)">
-        <h2>Sign Up:</h2>
-        <fieldset>
-          <input type="text" name="name" placeholder="Name:">
-        </fieldset>
-        <fieldset>
-          <input type="text" name="username" placeholder="Username:">
-        </fieldset>
-        <fieldset>
-          <input type="text" name="email" placeholder="Email:">
-        </fieldset>
-        <fieldset>
-          <input type="text" name="avatar" placeholder="Avatar:">
-        </fieldset>
-          <input type="hidden" name="role" value="user">
-        <fieldset>
+        <h2>The more we care, we share!</h2>
+        <h3>Tweeter</h3>
+
+          <input type="text" name="avatar" placeholder="Upload your avatar">   
+          <input type="text" name="name" placeholder="Enter your name">
+          <input type="text" name="email" placeholder="Email">
+          <input type="hidden" name="user_type" value="user">
           <input type="password" name="password" placeholder="password">
-        </fieldset>
+ 
         <button>Sign Up</button>
       </form>
+
+      <p>Already have an account?<span><li class=""onClick="renderLogin()">login</li>
+      </span></p>
+
     </section>
-  `
+  `;
 }
 
 function singUp(event) {
-  event.preventDefault()
-  const form = event.target
-  const data = Object.fromEntries(new FormData(form))
-  fetch('/api/users',{
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(data)
+  event.preventDefault();
+  const form = event.target;
+  const data = Object.fromEntries(new FormData(form));
+  fetch("/api/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
   })
-    .then(res => res.json())
-    .then(res => {
-      if(res.error) {
-        console.log(res.error)
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.error) {
+        console.log(res.error);
       } else {
-        state.loggedInUserName = res
-        renderPostList()
+        state.loggedInUserName = res;
+        renderPostList();
       }
-    })
+    });
 }
