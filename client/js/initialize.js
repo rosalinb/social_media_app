8785ff3ed31a983a3dc6b26c890e550398171095
Ikilzig <palvarezimaz@gmail.com>
2022-06-07 18:03:54 +1000

    some changes

--- client/js/initialize.js
+++ client/js/initialize.js
@@ -3,16 +3,14 @@ const state = {
   state: {},
 };
 
+// fetch("/api/profile")
+//   .then((res) => res.json())
+//   .then((profile) => {
+//     state.profile = profile;
+//     renderProfile();
+//   });
 
-fetch("/api/profile")
-  .then((res) => res.json())
-  .then((profile) => {
-    state.profile = profile;
-    renderProfile();
-  });
-
-
-fetch("/api/posts")
+fetch('/api/posts')
   .then((res) => res.json())
   .then((posts) => {
     state.posts = posts;
