58d7d0d07d13b71cec2ac8d5d2158712728f5e68
rosalinb <rosalinbiswal009@gmail.com>
2022-06-07 18:00:19 +1000

    home-page

--- client/js/initialize.js
+++ client/js/initialize.js
@@ -3,19 +3,17 @@ const state = {
   state: {},
 };
 
+// fetch("/api/profile")
+//   .then((res) => res.json())
+//   .then((profile) => {
+//     state.profile = profile;
+//     renderProfile();
+//   });
 
-fetch("/api/profile")
-  .then((res) => res.json())
-  .then((profile) => {
-    state.profile = profile;
-    renderProfile();
-  });
-
-
-fetch("/api/posts")
-  .then((res) => res.json())
-  .then((posts) => {
-    state.posts = posts;
-    renderPostList();
-    header();
-  });
+// fetch("/api/posts")
+//   .then((res) => res.json())
+//   .then((posts) => {
+//     state.posts = posts;
+//     renderPostList();
+//     header();
+//   });
