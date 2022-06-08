4798879a11d4f8b8a5e19089dc7ba14c6e6fdfea
Ikilzig <palvarezimaz@gmail.com>
2022-06-08 15:30:51 +1000

    profile kinda works

--- controllers/profiles_controller.js
+++ controllers/profiles_controller.js
@@ -7,12 +7,17 @@ const router = express.Router();
 const Profile = require('../models/profile');
 
 router.get('/', (req, res) => {
-  Profile.getProfileInfo(req.session.userId).then(
-    ({ avatar, name, email, about_you }) =>
-      res.json({ avatar, name, email, about_you })
+  Profile.getProfileInfo(req.session.userId).then((profile) =>
+    res.json(profile)
   );
-  // .then((profile) => (state.profile = profile));
 });
+
+// state.loggedInUserName = res;
+//     avatar, name, email, about_you }) =>
+//     res.json({ avatar, name, email, about_you })
+//   );
+//   // .then((profile) => (state.profile = profile));
+// });
 
 // })
 router.put('/', (req, res) => {
