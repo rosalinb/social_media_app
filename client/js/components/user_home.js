e3f7c9a0a36cf1dbc761617b1d1273a5c80f18da
rosalinb <rosalinbiswal009@gmail.com>
2022-06-08 15:35:06 +1000

    updating CSS

--- client/js/components/user_home.js
+++ client/js/components/user_home.js
@@ -1,14 +1,16 @@
 function renderUserHomePage() {
   if (state.loggedInUserName) {
     const page = document.querySelector("#page");
-
     page.innerHTML = `
     <div class="entry-page-wrapper">
+    <div class = "user-page-details">
+
   
      <h1>${navBar}</h1>
      <section class="entry-page-container">
 
-  <h1>${renderPostsList()}</h1>
-  </section>`;
+    <h1>${renderPostsList()}</h1>
+    </section>
+  </div>`;
   }
 }
