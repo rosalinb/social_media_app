909373bc6809048f4185887390fe40e476e4787e
Ikilzig <palvarezimaz@gmail.com>
2022-06-09 20:49:00 +1000

    login

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -1,11 +1,40 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 
 //models
-const User = require("../models/user");
+const User = require('../models/user');
 
-router.post("/", (req, res) => {
+// router.post('/', (req, res) => {
+//   const { email, password } = req.body;
+
+//   User.findByEmail(email).then((user) => {
+//     if (user === undefined) {
+//       res.json({ error: 'That user is unknown to us' });
+//     } else if (user) {
+//       const isValidPassword = bcrypt.compareSync(
+//         password,
+//         user.password_digest
+//       );
+
+//       if (user && isValidPassword) {
+//         req.session.userId = user.id;
+//         res.json({
+//           userId: user.id,
+//           userName: user.name,
+//           avatar: user.avatar,
+//           about_you: user.about_you,
+//           email: user.email,
+//         });
+//       } else {
+//         res.json({ error: 'Something is not right, please try again' });
+//       }
+//     }
+//   });
+// });
+
+///// WORKING VERSION
+router.post('/', (req, res) => {
   const { email, password } = req.body;
 
   User.findByEmail(email).then((user) => {
@@ -19,15 +48,17 @@ router.post("/", (req, res) => {
         about_you: user.about_you,
         email: user.email,
       });
+    } else {
+      res.json(null);
     }
   });
 });
 
-router.delete("/", (req, res) => {
+router.delete('/', (req, res) => {
   req.session.userId = undefined;
   // req.session.destroy();
   // res.clearCookie("user_sid");
-  res.json({ message: "logout successfully" });
+  res.json({ message: 'logout successfully' });
 });
 
 module.exports = router;
