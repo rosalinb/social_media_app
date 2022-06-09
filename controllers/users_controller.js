748b2036ab099cac609c1aa5058070aff4d2d882
Ikilzig <palvarezimaz@gmail.com>
2022-06-10 00:03:48 +1000

    login errors fixed

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 
 // models
-const User = require("../models/user");
+const User = require('../models/user');
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   const { avatar, name, email, user_type, password, about_you } = req.body;
   const passwordDigest = bcrypt.hashSync(
     password,
@@ -14,7 +14,7 @@ router.post("/", (req, res) => {
   );
 
   if (name.length < 2) {
-    res.status(400).json({ error: "name shoud be at least 8 characters" });
+    res.status(400).json({ error: 'name shoud be at least 8 characters' });
   } else {
     User.create(avatar, name, email, user_type, passwordDigest, about_you).then(
       (userName) => res.json(userName)
