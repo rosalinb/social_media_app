de5be90363ccfb60a9eb659943d15495520f3d5f
rosalinb <rosalinbiswal009@gmail.com>
2022-06-09 13:48:26 +1000

    edited login and added CSS avatar

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 //models
-const User = require('../models/user');
+const User = require("../models/user");
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const { email, password } = req.body;
 
   User.findByEmail(email).then((user) => {
@@ -19,17 +19,15 @@ router.post('/', (req, res) => {
         about_you: user.about_you,
         email: user.email,
       });
-    } else {
-      res.json({ error: 'Incorrect username or password' });
     }
   });
 });
 
-router.delete('/', (req, res) => {
+router.delete("/", (req, res) => {
   req.session.userId = undefined;
   // req.session.destroy();
   // res.clearCookie("user_sid");
-  res.json({ message: 'logout successfully' });
+  res.json({ message: "logout successfully" });
 });
 
 module.exports = router;
