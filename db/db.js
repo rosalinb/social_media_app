d8ba2c358a7003ffc19607f3a1d158bcc9cee777
Ikilzig <palvarezimaz@gmail.com>
2022-06-10 10:22:09 +1000

    ssl db

--- db/db.js
+++ db/db.js
@@ -4,9 +4,9 @@ let db;
 if (process.env.NODE_ENV === 'production') {
   db = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
-    // ssl: {
-    //   rejectUnauthorized: false
-    // }
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
 } else {
   db = new pg.Pool({
