7954c64022120478e0f8d10294c7bd92795bebea
Ikilzig <palvarezimaz@gmail.com>
2022-06-10 10:46:01 +1000

    css updated

--- client/js/components/post_page.js
+++ client/js/components/post_page.js
@@ -3,14 +3,23 @@ function renderPostPage() {
   let localTime = new Date().toLocaleTimeString();
   let allTime = `${localDate} at ${localTime}`;
   document.querySelector('.entry-page-container').innerHTML = `
-    <section>
+    <section class="post-new-tweets">
       <form onSubmit="createPost(event)">
-        <p>${state.loggedInUserName.userName}</p>
+      <h2>${state.loggedInUserName.userName}</h2>
+      <div>
         <label>What's happening?</label>
         <textarea name='post' rows='4' cols='50'></textarea>
-        <input name="attachment">Attachment</input>
+      </div>
+      <div>
+        <input name="attachment"></input>
+        <span class="material-icons attachment-icon">add_photo_alternate</span>
+      </div>
+      <div>
         <input name="timestamp" value="${allTime}" type="hidden"></input>
-        <button>Post</button>
+      </div>
+      <div>
+        <button class="post-tweets-button"><span class="material-icons post-tweets">check_circle</span></button>
+      <div>
       </form>
     </section>
   `;
